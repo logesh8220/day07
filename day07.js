7d7329eac48e9541861445b448c253459acced97
logesh8220 <106301040+logesh8220@users.noreply.github.com>
2022-07-08 14:12:49 +0530

    Update day07.js

--- day07.js
+++ day07.js
@@ -3,6 +3,8 @@
 
 //Uber Price Calculation
 
+
+
 class uber {
     constructor(distance,bookingfees){
         this.distance = distance;
@@ -23,8 +25,11 @@ let uberPrice = new ubercal (87,187)
 uberPrice.price();
 
 
+//--------------------------------------------------------------------------------------------------
 
 //Person Class
+
+
 
 class person {
     constructor(Name,age,phoneNo,area,street,pin){
@@ -54,3 +59,39 @@ class persondetials extends person {
 
 let obj =  new persondetials ("logesh",21,8220739131,"chennai","GandhiStreet",600025)
 obj.detials()
+
+//-------------------------------------------------------------------------------------------------------
+
+
+//Movie Rating 
+
+class movies {
+    constructor(title,studio,rating){
+          this.title = title;
+          this.studio = studio;
+          this.rating = rating;
+    }
+}
+class rating extends movies {
+    constructor(title,studio,rating){
+        super (title,studio,rating)
+    }
+
+    ratings () {
+        if(this.rating === "PG"){
+            console.log(`Movie Detials:
+            The movie is: ${this.title}
+            The studio is:${this.studio}
+            The rating is:${this.rating}`)
+        }
+        else {
+            console.log(`Movie Detials:
+            The movie is: ${this.title}
+            The studio is:${this.studio}
+            The rating is:${this.rating}`)
+        }
+    }
+}
+
+let movieobj =  new rating("Casino Royale","Eon Productions", "PG­13")
+movieobj.ratings()
